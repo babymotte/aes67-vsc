27cb9a8ed887c6beb9c1b765f7d4fe9cf18d8800
Michael Bachmann <mbachmann@bbmsoft.net>
2024-10-07 19:19:14 +0200

    fixed setting capabilities

--- debug.sh
+++ debug.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cargo build || exit $?
-sudo setcap cap_sys_nice+eip ./target/debug/aes67-vsc || exit $?
-sudo setcap cap_net_bind_service=ep ./target/debug/aes67-vsc || exit $?
+sudo setcap 'cap_net_bind_service+eip cap_sys_nice+eip' ./target/debug/aes67-vsc || exit $?
 ./target/debug/aes67-vsc "$@"
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cargo install --path . || exit $?
-sudo setcap cap_sys_nice+eip $(which aes67-vsc) || exit $?
-sudo setcap cap_net_bind_service=ep $(which aes67-vsc) || exit $?
+sudo setcap 'cap_net_bind_service+eip cap_sys_nice+eip' $(which aes67-vsc) || exit $?
